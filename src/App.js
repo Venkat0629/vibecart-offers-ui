7a3c0453e62c9c4c241d1affecc0168b74916b04
spullaganti <spullaganti@nisum.com>
2024-08-26 07:05:03 -0700

    [OFMS-1]:Created Git project skeleton of the website with initial screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { login, logout } from './components/Redux/authSlice';
+import Login from './components/Login/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateOffers from './components/Offers/CreateOffers';
+import UpdateOffers from './components/Offers/UpdateOffers';
+import DeleteOffers from './components/Offers/DeleteOffers';
+import Sidebar from './components/Sidebar/Sidebar';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import './App.css';  // Ensure this import
 
-function App() {
+const App = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    dispatch(login());
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Hello World</h1>
-      </header>
+    <div className='app-container'>
+      <Router>
+        <Header onLogout={handleLogout} isLoggedIn={isLoggedIn} />
+        <div className='content-container'>
+          {isLoggedIn ? (
+            <>
+              <div className='sidebar-container'>
+                <Sidebar />
+              </div>
+              <div className='main-content'>
+                <Routes>
+                  <Route path="/" element={<Navigate to="/dashboard" />} />
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/createOffers" element={<CreateOffers />} />
+                  <Route path="/updateOffers" element={<UpdateOffers />} />
+                  <Route path="/deleteOffers" element={<DeleteOffers />} />
+                  <Route path="*" element={<Navigate to="/dashboard" />} />
+                </Routes>
+              </div>
+            </>
+          ) : (
+            <Routes>
+              <Route path="/login" element={<Login onLogin={handleLogin} />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+          )}
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
