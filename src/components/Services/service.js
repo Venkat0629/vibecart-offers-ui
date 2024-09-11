e64ed990cb69c0d3e1a53acce381a7297e5c5379
vveera <vveera@nisum.com>
2024-09-11 19:29:58 +0530

    added ip config

--- src/components/Services/service.js
+++ src/components/Services/service.js
@@ -1,3 +1,1 @@
 export const VIBECART_URI = "http://10.3.45.15:4001";
-// export const ECOM_URI ="http://localhost:6060/vibecart/ecom";
-// export const ACCOUNT_SERVICE_URI = "http://localhost:6601/api/v1/vibe-cart/accounts";
